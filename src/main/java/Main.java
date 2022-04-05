68436a59252e32122cd2fd89b5ed16a88f2c7961
Gavrik <strn.stics@gmail.com>
2022-04-05 16:09:07 +0300

    Second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello MTFK");
         System.out.println("1pull");
+        System.out.println("2pull");
     }
 }
